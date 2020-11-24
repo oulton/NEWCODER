27a01d387d653386ea7e6adf153721fc884dd172
hyper <haipengzhu666@126.com>
2020-11-24 19:00:13 +0800

    JZ1

---
+++ JZ1_二维数组中的查找/JZ1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool Find(int target, vector<vector<int> > array) {
+	int row = 0;
+	int col = array[0].size();
+	while (row < array.size())
+	{
+		if (target > array[row][col - 1])
+			row++;
+		else if (target == array[row][col - 1])
+			return true;
+		else
+			col--;
+	}
+	cout << row << "   " << col << "  " << endl;
+	return false;
+}
+
+int main()
+{
+	int target = 13;
+	vector<vector<int>> array = {	{ 1, 2, 8, 9, 14 }, 
+									{ 2, 4, 9, 12, 15 }, 
+									{ 4, 6, 10, 13, 21 }, 
+									{ 6, 8, 11, 15, 25 } };
+	cout << Find(target, array) << endl;
+	system("pause");
+	return 0;
+}
